1ebf6c3ccf9cc4d2b614bf064d2e9a0f30747ca1
Yegor Bitensky <yegorbitensky@gmail.com>
2021-09-13 01:31:04 +0300

    Add send chat message view

--- django/src/chat/urls.py
+++ django/src/chat/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:room_name>/', views.room, name='room'),
+    path('send_message/<str:room_name>/<str:message>/', views.send_chat_message, name='send_chat_message'),
 ]
--- django/src/chat/views.py
+++ django/src/chat/views.py
@@ -1,3 +1,8 @@
+from asgiref.sync import async_to_sync
+
+from channels import layers
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -9,3 +14,15 @@ def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name': room_name
     })
+
+
+def send_chat_message(request, room_name, message):
+    channel_layer = layers.get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        'chat_%s' % room_name,
+        {
+            'type': 'chat_message',
+            'message': message
+        }
+    )
+    return HttpResponse()
