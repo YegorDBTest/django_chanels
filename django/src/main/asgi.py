ed2fca36497b34b128577348b3cdef9926c55244
Yegor Bitensky <yegorbitensky@gmail.com>
2021-10-12 21:14:29 +0300

    Add rest framework

--- django/src/main/asgi.py
+++ django/src/main/asgi.py
@@ -3,14 +3,16 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import chat.routing
+
+from chat.middleware import DRFAuthTokenMiddleware
+from chat.routing import websocket_urlpatterns
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
 application = ProtocolTypeRouter({
     # "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(chat.routing.websocket_urlpatterns)
-    ),
+    "websocket": AuthMiddlewareStack(DRFAuthTokenMiddleware(
+        URLRouter(websocket_urlpatterns)
+    )),
 })
--- django/src/main/settings.py
+++ django/src/main/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -138,4 +140,14 @@ CHANNEL_LAYERS = {
             "hosts": [('redis', 6379)],
         },
     },
+}
+
+
+# REST
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
 }
