3982223f33f7de0eb7b72a7af6c16b518be01345
Yegor Bitensky <yegorbitensky@gmail.com>
2021-10-15 18:48:30 +0300

    Add daphne usage

--- django/src/entrypoint_chanels.sh
+++ django/src/entrypoint_chanels.sh
@@ -4,4 +4,6 @@
 # 	sleep 60
 # done
 
-python manage.py runserver 0:8000;
+# python manage.py runserver 0:8000;
+
+daphne -b 0.0.0.0 -p 8000 main.asgi:application;
--- django/src/main/asgi.py
+++ django/src/main/asgi.py
@@ -4,14 +4,17 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
 from chat.middleware import DRFAuthTokenMiddleware
 from chat.routing import websocket_urlpatterns
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
-
 application = ProtocolTypeRouter({
-    # "http": get_asgi_application(),
+    # "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(DRFAuthTokenMiddleware(
         URLRouter(websocket_urlpatterns)
     )),
