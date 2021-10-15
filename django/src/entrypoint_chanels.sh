9a7e7693ca153af19124edc2f5c30fc12b47bce7
Yegor Bitensky <yegorbitensky@gmail.com>
2021-10-15 21:22:43 +0300

    Add uvicorn usage

--- django/src/entrypoint_chanels.sh
+++ django/src/entrypoint_chanels.sh
@@ -6,4 +6,6 @@
 
 # python manage.py runserver 0:8000;
 
-daphne -b 0.0.0.0 -p 8000 main.asgi:application;
+# daphne -b 0.0.0.0 -p 8000 main.asgi:application;
+
+uvicorn --host=0.0.0.0 --port=8000 --workers=`nproc` main.asgi:application;
