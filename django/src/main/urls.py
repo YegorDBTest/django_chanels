f05b17295c888ecc8c9bee4cc435eb978f6a05f9
Yegor Bitensky <yegorbitensky@gmail.com>
2022-01-23 23:16:01 +0300

    Add Simple JWT

--- django/src/main/urls.py
+++ django/src/main/urls.py
@@ -2,8 +2,13 @@ from django.conf.urls import include
 from django.urls import path
 from django.contrib import admin
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     path('chat/', include('chat.urls')),
     path('admin/', admin.site.urls),
+
+    path('api/jwt/token/', TokenObtainPairView.as_view(), name='jwt_token_obtain_pair'),
+    path('api/jwt/token/refresh/', TokenRefreshView.as_view(), name='jwt_token_refresh'),
 ]
